44fc25ae4f397efc7ea1b4776ebc9e8cf6f8db4a
karannchaudhari04 <karanchaudhari34804@gmail.com>
2025-07-01 20:01:26 +0530

    initial commit

--- week-1/functions.js
+++ week-1/functions.js
@@ -1,9 +1,36 @@
-function sum(a,b){
+//This is simple function example that takes two numbers.
+
+function Findsum(a, b){
     return a + b;
-    
 }
 
-let value = sum(2,5);
-let value2 = sum(3,4);
-console.log(value2);
+
+let value = Findsum(2,5);
+let value2 = Findsum(3,4);
+
+
 console.log(value);
+console.log(value2);
+
+
+
+
+
+
+
+//This is a function callback example. Where you can call a function inside another function.
+function sum(num1, num2, callfunc){
+    let result = num1 + num2;
+    callfunc(result);
+}
+
+
+function displaySum(data){
+    console.log("The sum is: " + data);
+}
+
+function sumDisplay(data){
+    console.log("The sum is: " + data);
+}
+
+let ans = sum(12,5, displaySum);
