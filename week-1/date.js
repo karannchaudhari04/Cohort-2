c8084d1b8fe2b7c5398400f670e9287def112c47
karannchaudhari04 <karanchaudhari34804@gmail.com>
2025-07-04 11:46:10 +0530

    initial commit

---
+++ week-1/date.js
@@ -0,0 +1,5 @@
+const currentDate = new Date;
+console.log(currentDate.getHours() + ':' + currentDate.getMinutes() + ':' + currentDate.getSeconds());
+console.log(currentDate.getDate());
+console.log(currentDate.getMonth() + 1);
+console.log(currentDate.getFullYear());
---
+++ week-1/math.js
@@ -0,0 +1,10 @@
+console.log(Math.random());
+console.log(Math.PI);
+console.log(Math.sqrt(16)); 
+console.log(Math.pow(2, 3));
+console.log(Math.max(10, 20, 30));
+console.log(Math.min(10, 20, 30));
+console.log(Math.floor(4.7));
+console.log(Math.ceil(4.1));
+console.log(Math.round(4.5));
+console.log(Math.abs(-5));
